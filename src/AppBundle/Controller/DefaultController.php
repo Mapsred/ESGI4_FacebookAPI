5771f8faa994efe639450846446cf26c5e8dd288
François MATHIEU <francois.mathieu@livexp.fr>
2017-12-04 12:38:29 +0100

    Add HWIOAuth for facebook

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,17 +2,28 @@
 
 namespace AppBundle\Controller;
 
+use HWI\Bundle\OAuthBundle\Security\Core\User\OAuthUser;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * Class DefaultController
+ *
+ * @author François MATHIEU <francois.mathieu@livexp.fr>
+ * @method OAuthUser getUser()
+ */
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @param Request $request
+     * @return Response
      */
     public function indexAction(Request $request)
     {
+        var_dump($this->getUser());
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
---
+++ src/AppBundle/Controller/OAuthController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: francois.mathieu
+ * Date: 04/12/2017
+ * Time: 12:30
+ */
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class OAuthController extends Controller
+{
+
+}
