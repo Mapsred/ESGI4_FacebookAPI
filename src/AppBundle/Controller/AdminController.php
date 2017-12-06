3a49d972ee66ac2b9e144ffecebd1d22cbcd5423
François MATHIEU <francois.mathieu@livexp.fr>
2017-12-06 09:56:16 +0100

    Add color pages

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -9,11 +9,13 @@
 namespace AppBundle\Controller;
 
 
+use AppBundle\Entity\Site;
 use AppBundle\Security\Core\User\OAuthUser;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * Class AdminController
@@ -25,9 +27,30 @@ class AdminController extends Controller
 {
     /**
      * @Route("/admin", name="admin_index")
+     * @return Response
      */
     public function indexAction()
     {
-        return $this->render('AppBundle:Admin:index.html.twig');
+        $site = $this->findSiteOrThrowException();
+
+        return $this->render('AppBundle:Admin:index.html.twig', [
+            'site' => $site
+        ]);
     }
+
+    /**
+     * @return Site|null|object
+     * @throws NotFoundHttpException
+     */
+    private function findSiteOrThrowException()
+    {
+        if (null !== $site = $this->getDoctrine()->getRepository("AppBundle:Site")->findOneBy(['userId' => $this->getUser()->getId()])) {
+            $site->setOAuthUser($this->getUser());
+
+            return $site;
+        }
+
+        throw $this->createNotFoundException("Site $site not found");
+    }
+
 }
